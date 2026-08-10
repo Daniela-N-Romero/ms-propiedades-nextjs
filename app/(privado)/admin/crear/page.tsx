234e5f2b27bf0174e0e569d3fc3211fe3361b674
Daniela Romero <danielanahirromero@gmail.com>
2026-08-10 12:54:42 -0300

    agregue env de analytics y arregle build

--- app/(privado)/admin/crear/page.tsx
+++ app/(privado)/admin/crear/page.tsx
@@ -21,6 +21,7 @@ export default async function CrearPropiedadPage() {
         propietarios ={propietarios}
         colegas ={colegas}              
         />
+        
         </Suspense>
     </div>
   );
